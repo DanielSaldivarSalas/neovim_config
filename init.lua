98fa36b0c3a1995c6048db25a66f94f05c1e53d2
Daniel Saldivar <daniel.saldivar.ds@gmail.com>
2022-10-27 19:23:15 -0400

    set up colorscheme and plugin manager

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
+require "user.plugins-setup" --this should be on top
 require "user.core.options"
 require "user.core.colorscheme"
+
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 
--- lua/user/core/mappings.lua
+++ lua/user/core/mappings.lua
@@ -1,3 +1,7 @@
 local function map(m,k,v)
 	vim.keymap.set(m,k,v, { silent = true })
 end
+
+local g = vim.g
+
+g.mapleader = " "
