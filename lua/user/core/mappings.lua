0910959c33d026aeb473b8097511c21fd67fd5cd
Daniel Saldivar <daniel.saldivar.ds@gmail.com>
2022-10-28 14:10:11 -0400

    added snippets

--- lua/user/core/mappings.lua
+++ lua/user/core/mappings.lua
@@ -11,12 +11,6 @@ g.mapleader = " "
 map("n", "<leader>h", ":nohlsearch<CR>")
 
 
--- Packer
-map("n", "<leader>pc", "<cmd>PackerSync<cr>")
-map("n", "<leader>pi", "<cmd>PackerInstall<cr>")
-map("n", "<leader>ps", "<cmd>PackerSync<cr>")
-
-
 
 -- Neotree
 map("n", "<leader>e", "<cmd>Neotree toggle<cr>") -- Toggle Explorer
