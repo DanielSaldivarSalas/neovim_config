50b653c1776fb2f9eca5c89e4bed930786c4c79f
Daniel Saldivar <daniel.saldivar.ds@gmail.com>
2022-11-03 14:06:33 -0400

    changed icons for windows

--- lua/user/plugins/bufferline.lua
+++ lua/user/plugins/bufferline.lua
@@ -8,7 +8,14 @@ require("bufferline").setup{}
 else
     require("bufferline").setup{
         options = {
-            close_icon = "x"
+            buffer_close_icon = "",
+            -- buffer_close_icon = '',
+            modified_icon = "●",
+            close_icon = "",
+    -- close_icon = '',
+            left_trunc_marker = "",
+            right_trunc_marker = "",
+
         }
     }
 end
